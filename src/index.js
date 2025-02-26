b20beee3d17e5e40e8eb734db162f4c0fc8f2033
阿男 <l.weinan@gmail.com>
2025-02-27 00:30:23 +0800

    add other

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+function Other() {
+    return (
+        <div>Other</div>
+    )
+}
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+const app = createRoot(document.getElementById('app'));
+app.render(<App />);
+
+const other = createRoot(document.getElementById('other'));
+other.render(<Other />);
