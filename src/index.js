0b2a5e29eed36ade67e5918aebfb1db05bb0c010
阿男 <l.weinan@gmail.com>
2025-03-07 08:34:06 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
 import React, {Fragment} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-//
-// function Other() {
-//     return (
-//         <div>Other</div>
-//     )
-// }
-//
-// function Two() {
-//     return (
-//         <div>Two</div>
-//     )
-// }
-//
-// function Third() {
-//     return (
-//         <div>Third</div>
-//     )
-// }
-//
-// function TheFrag() {
-//     return (
-//         <Fragment>
-//             <Two />
-//             <Third />
-//         </Fragment>
-//     )
-// }
+
 
 const app = createRoot(document.getElementById('app'));
 app.render(<App />);
-//
-// const other = createRoot(document.getElementById('other'));
-// other.render(<Other />);
-//
-// const theFrag = createRoot(document.getElementById('theFrag'));
-// theFrag.render(<TheFrag />);
