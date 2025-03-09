fd2ad36c3040c7c89ec806db9376b5a27bdec018
阿男 <l.weinan@gmail.com>
2025-03-09 23:07:05 +0800

    refactor

--- my-app/src/form/MyForm.jsx
+++ my-app/src/form/MyForm.jsx
@@ -1,4 +1,23 @@
 export default function MyForm(props) {
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formEl = event.currentTarget
+        const formData = new FormData(formEl)
+        const firstName = formData.get("firstName")
+        const lastName = formData.get("lastName")
+        submitViaAPI({
+            firstName,
+            lastName,
+        })
+        formEl.reset()
+    }
+
+    function submitViaAPI({firstName, lastName}) {
+        console.log(firstName, lastName)
+        console.log("Submitting via API...")
+    }
+
     return (
         <form id="my-form" onSubmit={handleSubmit}>
             <p>
@@ -12,22 +31,4 @@ export default function MyForm(props) {
             </p>
         </form>
     )
-}
-
-function handleSubmit(event) {
-    event.preventDefault()
-    const formEl = event.currentTarget
-    const formData = new FormData(formEl)
-    const firstName = formData.get("firstName")
-    const lastName = formData.get("lastName")
-    submitViaAPI({
-        firstName,
-        lastName,
-    })
-    formEl.reset()
-}
-
-function submitViaAPI({firstName, lastName}) {
-    console.log(firstName, lastName)
-    console.log("Submitting via API...")
 }
