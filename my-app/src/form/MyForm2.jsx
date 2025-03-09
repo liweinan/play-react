1656bec7b19352784e0015a28127170af9063534
阿男 <l.weinan@gmail.com>
2025-03-09 22:27:33 +0800

    add data

--- my-app/src/form/MyForm2.jsx
+++ my-app/src/form/MyForm2.jsx
@@ -1,32 +1,42 @@
+import {useState} from "react";
+
+
+
 export default function MyForm2(props) {
+    const [items, setItems] = useState([])
+
+    function handleAction(formData) {
+        const foo = formData.get("foo")
+        const bar = formData.get("bar")
+        submitViaAPI({
+            foo,
+            bar,
+        })
+    }
+
+    function submitViaAPI({foo, bar}) {
+        console.log(foo, bar)
+        console.log("Submitting via API...")
+        setItems(prev => [...prev, foo, bar])
+    }
+
     return (
-        <form id="my-form2" action={handleAction}>
+        <>
+            <form id="my-form2" action={handleAction}>
+                <p>
+                    <input name="foo" type="text" placeholder="Foo"/>
+                </p>
+                <p>
+                    <input name="bar" type="text" placeholder="Bar"/>
+                </p>
+                <p>
+                    <button type="submit">Submit</button>
+                </p>
+            </form>
             <p>
-                <input name="foo" type="text" placeholder="Foo"/>
+                {items}
             </p>
-            <p>
-                <input name="bar" type="text" placeholder="Bar"/>
-            </p>
-            <p>
-                <button type="submit">Submit</button>
-            </p>
-        </form>
+        </>
     )
 }
 
-
-function handleAction(formData) {
-    const foo = formData.get("foo")
-    const bar = formData.get("bar")
-    console.log(foo)
-    console.log(bar)
-    submitViaAPI({
-        foo,
-        bar,
-    })
-}
-
-function submitViaAPI({firstName, lastName}) {
-    console.log(firstName, lastName)
-    console.log("Submitting via API...")
-}
