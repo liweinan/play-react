24cae78f092aeb2bc3ab17204937e41e609437db
阿男 <l.weinan@gmail.com>
2025-03-09 23:22:30 +0800

    add checkboxes

--- my-app/src/form/MyForm2.jsx
+++ my-app/src/form/MyForm2.jsx
@@ -1,7 +1,5 @@
 import {useState} from "react";
 
-
-
 export default function MyForm2(props) {
     const [items, setItems] = useState([])
 
@@ -9,36 +7,59 @@ export default function MyForm2(props) {
         <li key={item}>{item}</li>
     ))
 
+    const [animals, setAnimals] = useState([])
+
+    const animalList = animals.map(animal => (
+        <li key={animal}>{animal}</li>
+    ))
+
     function handleAction(formData) {
         const foo = formData.get("foo")
-        const bar = formData.get("bar")
+        const animals = formData.getAll("animals")
         submitViaAPI({
             foo,
-            bar,
+            animals,
         })
     }
 
-    function submitViaAPI({foo, bar}) {
-        console.log(foo, bar)
+    function submitViaAPI({foo, animals}) {
+        console.log(foo)
+        console.log(animals)
         console.log("Submitting via API...")
-        setItems(prev => [...prev, foo, bar])
+        setItems(prev => [foo])
+        setAnimals(prev => animals)
     }
 
     return (
         <>
             <form id="my-form2" action={handleAction}>
                 <p>
-                    <input name="foo" type="text" placeholder="Foo"/>
+                    <input key="foo" name="foo" type="text" placeholder="Foo"/>
                 </p>
                 <p>
-                    <input name="bar" type="text" placeholder="Bar"/>
+                    {itemList}
                 </p>
+                <fieldset>
+                    <legend>Animal</legend>
+                    <label>
+                        <input name="animals" type="checkbox" value="cat" />
+                        Cat
+                    </label>
+                    <label>
+                        <input name="animals" type="checkbox" value="dog" />
+                        Dog
+                    </label>
+                    <label>
+                        <input name="animals" type="checkbox" value="fish" />
+                        Fish
+                    </label>
+                </fieldset>
                 <p>
                     <button type="submit">Submit</button>
                 </p>
             </form>
             <p>
-                {itemList}
+                {animalList}
             </p>
         </>
     )
