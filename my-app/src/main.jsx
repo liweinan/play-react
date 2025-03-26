2f86c0608a8a0fe81dd664bd0cee1a10dcb4a191
moicen <moicen@foxmail.com>
2025-03-27 00:54:36 +0800

    update

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PriceCalculator from './PriceCalculator.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PriceCalculator />
   </StrictMode>,
 )
