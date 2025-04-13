c6304920d2f8830669ce1ed7e0c052c4413cbedf
阿男 <l.weinan@gmail.com>
2025-04-13 23:18:59 +0800

    refactor to use link

---
+++ my-app/src/pages/Forms.jsx
@@ -0,0 +1,14 @@
+import MyForm from "../form/MyForm"
+import MyForm2 from "../form/MyForm2"
+
+export default function Forms() {
+  return (
+    <div>
+      <h2>Forms</h2>
+      <div className="forms-container">
+        <MyForm/>
+        <MyForm2/>
+      </div>
+    </div>
+  )
+} 
